47121280fff056b30484340dc2056244151a7d31
elisarrtech <146400511+elisarrtech@users.noreply.github.com>
2025-07-16 01:43:31 -0600

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
     setToken(null);
 
     try {
-      const response = await fetch('https://quickflow-nxg1.onrender.com/api/login', {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
