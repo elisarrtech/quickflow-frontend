c4fc7c8b14b7c20859dce946555a60546104e760
elisarrtech <146400511+elisarrtech@users.noreply.github.com>
2025-07-16 23:22:59 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
-import Tareas from './pages/Tareas'; // ✅
+import Tareas from './pages/Tareas';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Login />} /> {/* ✅ Agregado */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/tareas" element={<Tareas />} /> {/* ✅ */}
+        <Route path="/tareas" element={<Tareas />} />
       </Routes>
     </BrowserRouter>
   );
