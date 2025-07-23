1158688ebdc5bfa1bdd2564785e20cc262f9d2a7
elisarrtech <146400511+elisarrtech@users.noreply.github.com>
2025-07-23 01:06:33 -0600

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,6 +19,7 @@ const handleLogin = async (e) => {
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ email, password }),
   });
+
     
 
     // Validar que la respuesta sea JSON
