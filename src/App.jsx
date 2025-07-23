dc1e5fd99685645312fac4e8817e5ecd4949824d
elisarrtech <146400511+elisarrtech@users.noreply.github.com>
2025-07-23 13:48:04 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import Tareas from './pages/Tareas'; // ✅ Importación correcta
+import Estadisticas from './components/Estadisticas';
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/tareas" element={<Tareas />} /> {/* Página separada */}
+        <Route path="/estadisticas" element={<Estadisticas />} />
+      
       </Routes>
     </BrowserRouter>
   );
