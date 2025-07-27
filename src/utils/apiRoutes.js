1759e7eb25cf71f2b8fa368d8117afc70f968863
elisarrtech <146400511+elisarrtech@users.noreply.github.com>
2025-07-27 01:34:33 -0600

    Update apiRoutes.js

--- src/utils/apiRoutes.js
+++ src/utils/apiRoutes.js
@@ -1,1 +1,12 @@
+// src/utils/apiRoutes.js
+const API = import.meta.env.VITE_API_URL;
+
+export const RUTAS_API = {
+  tareas: `${API}/api/tareas`,
+  eventos: `${API}/api/eventos`,
+  perfil: `${API}/api/perfil`,
+  login: `${API}/api/login`,
+  register: `${API}/api/register`,
+  subirArchivo: `${API}/api/upload`,
+};
 
