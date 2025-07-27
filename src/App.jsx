c50dbdc3dd4c86284b8573df5968bf859f54d297
elisarrtech <146400511+elisarrtech@users.noreply.github.com>
2025-07-26 22:49:00 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DashboardLayout from './components/DashboardLayout';
+import Dashboard from './pages/Dashboard';
 import Tareas from './pages/Tareas';
 import Eventos from './components/Eventos';
 import Estadisticas from './components/Estadisticas';
 import Perfil from './pages/Perfil';
-import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <Router>
       <DashboardLayout>
         <Routes>
-          
-  </DashboardLayout>
-</Router>
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/tareas" element={<Tareas />} /> {/* Solo una ruta para Tareas */}
+          <Route path="/tareas" element={<Tareas />} />
           <Route path="/eventos" element={<Eventos />} />
           <Route path="/estadisticas" element={<Estadisticas />} />
           <Route path="/perfil" element={<Perfil />} />
